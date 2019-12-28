505f08f8fea73071d42073a643e5401cc79dd54d
Eaxarru <50877760+Eaxarru@users.noreply.github.com>
2019-12-28 19:19:50 -0300

    Instalando Slim Framework
    Instalando e testando Slim Framework

--- index.php
+++ index.php
@@ -32,6 +32,23 @@
   <i class="fal fa-user"></i> <!-- uses light style -->
   <!--brand icon-->
   <i class="fab fa-github-square"></i> <!-- uses brands style -->
+
+  <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->run();
+?>
     
 
     <!-- Optional JavaScript -->

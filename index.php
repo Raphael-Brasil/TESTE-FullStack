9fea5ee0d43794e938b677d3b1789d4e4e226579
Eaxarru <50877760+Eaxarru@users.noreply.github.com>
2019-12-28 17:51:17 -0300

    Instalação AngularJS
    Instalação e teste do AngularJs e do Bosstrap

--- index.php
+++ index.php
@@ -1,17 +1,32 @@
 <!doctype html>
-<html lang="en">
+<html ng-app lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <!-- Angularjs -->
+    <script src="angularjs/angular.min.js"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title>TESTE – VAGA DE DESENVOLVEDOR FULL-STACK</title>
   </head>
   <body>
-    <h1>Operacional</h1>
+  <div>
+    <label>Teste: </label>
+    <input type="text" ng-model="yourName" placegolder="Enter a name here">
+    <hr>
+    <h1>Hello {{yourName}}!</h1>
+  </div>
+  <div class="container">
+  <div class="row row-cols-3">
+    <div class="col">Column</div>
+    <div class="col">{{yourName}}</div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+  </div>
+</div>
+    
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->

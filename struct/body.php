9337daeb4f2259df9a69de06bfed5f437b1ddc24
Eaxarru <50877760+Eaxarru@users.noreply.github.com>
2019-12-28 23:38:33 -0300

    Organizacion

---
+++ struct/body.php
@@ -0,0 +1,184 @@
+  <!-- Corpo todo -->
+  <div class="container">
+  
+  <!-- Quatros itens de cima -->
+  <div class="row">
+
+  <!-- Primeiro item -->
+    <div class="col">    
+    <div class="card bg-light mb-3" style="max-width: 18rem;">
+  <div class="Title"  style="
+  background-color: #5A29FD;
+  ">Performance</div>
+  <div class="iconbk" style="            
+          background-color: #0D0133;
+       ">
+  <i class="far fa-check-square" style="
+            font-size: 7rem; 
+  "></i>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Checkin & Checkout</h5>
+    <p class="card-text">+ R$ X por colaborador por mês</p>
+    <a href="#" class="btn">Contratar</a>
+  </div>
+</div>    
+    </div>
+  
+   <!-- Segundo item -->  
+    <div class="col">    
+    <div class="card bg-light mb-3" style="max-width: 18rem;">
+  <div class="Title"  style="
+  background-color: #5C099C;
+  ">Engajamento</div>
+  <div class="iconbk" style="            
+          background-color: #433D5A;
+       ">
+  <i class="fas fa-certificate" style="
+  font-size: 7rem; 
+  ">  
+    </i>
+</div>
+  <div class="card-body">
+    <h5 class="card-title">Badges & Cards de Reconhecimento</h5>
+    <p class="card-text">Gratuito</p>
+    <a href="#" class="btn">Ativar</a>
+  </div>
+</div>    
+    </div>
+
+<!-- Terceiro item -->      
+    <div class="col">    
+    <div class="card bg-light mb-3" style="max-width: 18rem;">
+  <div class="Title"  style="
+  background-color: #5C099C;
+  ">Engajamento</div>
+  <div class="iconbk" style="            
+          background-color: #4C4551;
+       ">
+  <i class="fas fa-clipboard-list" style="
+  font-size: 7rem; 
+  "></i>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Planos de ação</h5>
+    <p class="card-text">Gratuito</p>
+    <a href="#" class="btn">Ativar</a>
+  </div>
+</div>    
+    </div>
+
+
+<!-- Quarto item -->      
+    <div class="col">    
+    <div class="card bg-light mb-3" style="max-width: 18rem;">
+  <div class="Title"  style="
+  background-color: #5C099C;
+  ">Engajamento</div>
+  <div class="iconbk" style="            
+          background-color: #8E6127;
+       ">
+  <i class="far fa-bookmark" style="
+  font-size: 7rem;
+  "></i>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Recomendações de Conteúdo sob medida</h5>
+    <p class="card-text"> + R$ X por colaborador por mês</p>
+    <a href="#" class="btn">Contratar</a>
+  </div>
+</div>    
+    </div>
+  </div> <!-- Div da row de cima --> 
+
+
+<!-- Quatros itens de baixo -->     
+  <div class="row">
+
+<!-- Quinto item -->  
+    <div class="col">    
+    <div class="card bg-light mb-3" style="max-width: 18rem;">
+  <div class="Title"  style="
+  background-color: #63842B;
+  ">Cultura</div>
+ <div class="iconbk" style="            
+          background-color: #DA5E00;
+       ">
+  <i class="fab fa-jenkins" style="
+  font-size: 7rem;
+  "></i>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Fitting Cultural</h5>
+    <p class="card-text"> + R$ X por colaborador por mês</p>
+    <a href="#" class="btn">Contratar</a>
+  </div>
+</div>    
+    </div>
+
+<!-- Sexto item -->      
+    <div class="col">    
+    <div class="card bg-light mb-3" style="max-width: 18rem;">
+  <div class="Title"  style="
+  background-color: #DA5E00;
+  ">Relações</div>
+  <div class="iconbk" style="            
+          background-color: #7C7B7A;
+       ">
+  <i class="fas fa-hands-helping" style="
+  font-size: 7rem;
+  "></i>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Assessments e Recomendações</h5>
+    <p class="card-text"> Gratuito </p>
+    <a href="#" class="btn">Ativar</a>
+  </div>
+</div>    
+    </div>
+
+<!-- Setimo item -->      
+    <div class="col">    
+    <div class="card bg-light mb-3" style="max-width: 18rem;">
+  <div class="Title"  style="
+  background-color: #5A29FD;
+  ">Perfomance</div>
+  <div class="iconbk" style="            
+          background-color: #00A2FF;
+       ">
+  <i class="far fa-file" style="
+  font-size: 7rem;
+  "></i>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Quick Deck</h5>
+    <p class="card-text"> + R$ X por colaborador por mês</p>
+    <a href="#" class="btn">Contratar</a>
+  </div>
+</div>    
+    </div>
+
+<!-- Oitavo item -->     
+    <div class="col">    
+    <div class="card bg-light mb-3" style="max-width: 18rem;">
+  <div class="Title"  style="
+  background-color: #5A29FD;
+  ">Perfomance</div>
+<div class="iconbk" style="            
+          background-color: #050962;
+       ">
+  <i class="fas fa-shoe-prints" style="
+  font-size: 7rem;
+  "></i>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Evidências & Feedback</h5>
+    <p class="card-text"> + R$ X por colaborador por mês</p>
+    <a href="#" class="btn">Contratar</a>
+  </div>
+</div>    
+    </div>
+
+  </div> <!-- Div da row de baixo --> 
+</div><!-- Div do corpo --> 
+   
---
+++ struct/head.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html ng-app lang="en">
+  <head>    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Angularjs -->
+    <script src="angularjs/angular.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <link href="/fontawesome/css/all.css" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="/css/layout.css">
+    <title>TESTE – VAGA DE DESENVOLVEDOR FULL-STACK</title>
+  </head>
+  <body>
+ 
